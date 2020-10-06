215ed170c42785296ec40bd063e1ded1e29da989
Gabriela Mendes Pereira Irmão <gabriela.mendes@C02SR23HH3QF.local>
2020-10-06 09:06:48 -0300

    changes

--- src/lib/alert.ts
+++ src/lib/alert.ts
@@ -3,11 +3,34 @@ import { Options } from '../types/options';
 
 export const softAlert = (options: Options) => {
   let body: HTMLBodyElement = document.querySelector('body');
-  body.innerHTML += html;
+  body.innerHTML += `
+  <div
+  class="soft-alert"
+  style="
+    background-color: burlywood;
+    border: 1px solid black;
+    position: fixed;
+    height: 300px;
+    width: 300px;
+    top: 0;
+    margin: 0 auto;
+  "
+  id="soft-alert"
+  >
+    <div class="soft-alert-title" id="soft-alert-title"></div>
+    <div class="soft-alert-message" id="soft-alert-message"></div>
+    <div class="soft-alert-actions" id="soft-alert-actions">
+      <button>OK</button>
+    </div>
+  </div>  
+  `;
   const title: HTMLDivElement = document.querySelector('#soft-alert-title');
   const message: HTMLDivElement = document.querySelector('#soft-alert-message');
   const actions: HTMLDivElement = document.querySelector('#soft-alert-actions');
   title.innerText += options.title;
   message.innerText += options.message;
   actions.style.display = options.actions ? 'block' : 'none';
+  actions.children[0].addEventListener('click', () => {
+    document.querySelector('#soft-alert').remove();
+  });
 };
